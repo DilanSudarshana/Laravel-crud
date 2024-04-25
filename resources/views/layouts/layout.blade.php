cdcfccf9585fb9d6db89c794c10f6a3fd8c61676
DilanSudarshana <dilanlakshan18@gmail.com>
2024-04-25 21:49:39 +0530

    design Footer

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -73,60 +73,76 @@
 
     @yield('content')
 
+    <style>
+        .image-container {
+            overflow: hidden;
+        }
+    
+        .image-container img {
+            transition: transform 0.5s ease;
+        }
+    
+        .image-container:hover img {
+            transform: translateX(100%);
+        }
+    </style>
+    
     <footer class="bg-body-tertiary text-center">
         <!-- Grid container -->
         <div class="container p-4">
             <!-- Section: Images -->
             <section class="">
                 <div class="row">
-                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded"
-                            data-ripple-color="light">
+                    <!-- First Row of Images -->
+                    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 image-container">
+                        <!-- Image 1 -->
+                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded" data-ripple-color="light">
                             <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" class="w-100" />
                             <a href="#!">
                                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded"
-                            data-ripple-color="light">
+                    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 image-container">
+                        <!-- Image 2 -->
+                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded" data-ripple-color="light">
                             <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/111.webp" class="w-100" />
                             <a href="#!">
                                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded"
-                            data-ripple-color="light">
+                    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 image-container">
+                        <!-- Image 3 -->
+                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded" data-ripple-color="light">
                             <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/112.webp" class="w-100" />
                             <a href="#!">
                                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded"
-                            data-ripple-color="light">
+                    <!-- Second Row of Images -->
+                    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 image-container">
+                        <!-- Image 4 -->
+                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded" data-ripple-color="light">
                             <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/114.webp" class="w-100" />
                             <a href="#!">
                                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded"
-                            data-ripple-color="light">
+                    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 image-container">
+                        <!-- Image 5 -->
+                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded" data-ripple-color="light">
                             <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/115.webp" class="w-100" />
                             <a href="#!">
                                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded"
-                            data-ripple-color="light">
+                    <div class="col-lg-2 col-md-6 mb-4 mb-md-0 image-container">
+                        <!-- Image 6 -->
+                        <div data-mdb-ripple-init class="bg-image hover-overlay shadow-1-strong rounded" data-ripple-color="light">
                             <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp" class="w-100" />
                             <a href="#!">
                                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
@@ -138,18 +154,14 @@
             <!-- Section: Images -->
         </div>
         <!-- Grid container -->
-
+    
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
             © 2024 cool blog post, Inc
         </div>
         <!-- Copyright -->
     </footer>
-
-
-
-
-    </footer>
+      
 </body>
 
 </html>
